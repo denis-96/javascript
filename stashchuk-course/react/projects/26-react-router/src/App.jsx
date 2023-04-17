08b812dc5093438f13bbd875960fab9cf0d7ea5b
Denis <denis.bargan2006@gmail.com>
2023-04-17 21:51:41 +0300

    Restructured

---
+++ stashchuk-course/react/projects/26-react-router/src/App.jsx
@@ -0,0 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contacts from "./components/Contacts";
+import NotFound from "./components/NotFound";
+import MainLayout from "./layouts/MainLayout";
+
+import "./App.css";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="contacts" element={<Contacts />} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
